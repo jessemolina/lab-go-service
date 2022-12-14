648b3f0ab9c9bc96291cfbce2acac82a6913fa45
Jesse Molina <jesse@jessemolina.xyz>
2022-12-14 16:22:17 -0600

    Added logger middleware.

--- cmd/services/service-api/handlers/handlers.go
+++ cmd/services/service-api/handlers/handlers.go
@@ -9,8 +9,10 @@ import (
 	"github.com/jessemolina/lab-go-service/cmd/services/service-api/handlers/debug/checkgrp"
 	"github.com/jessemolina/lab-go-service/cmd/services/service-api/handlers/v1/testgrp"
 	"github.com/jessemolina/lab-go-service/pkg/web"
+	"github.com/jessemolina/lab-go-service/internal/web/mid"
 	"go.uber.org/zap"
 )
+
 
 // ================================================================
 // FUNCTIONS
@@ -52,6 +54,7 @@ func DebugMux(build string, log *zap.SugaredLogger) http.Handler {
 func APIMux(cfg APIMuxConfig) *web.App {
 	app := web.NewApp(
 		cfg.Shutdown,
+		mid.Logger(cfg.Log),
 	)
 
 	v1(app, cfg)
